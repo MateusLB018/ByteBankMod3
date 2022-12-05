c61952ceb0f7252221242aff80b3df8c4bc4a22b
Mateu <Mateu@USUARIO>
2022-12-05 11:51:53 -0300

    Adicionar arquivos de projeto.

---
+++ ByteBankMod3/Funcionários/Funcionario.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBankMod3
+{
+   public  class Funcionario
+    {
+        
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public double Salario { get; set; }
+      
+       
+
+        public double GetBonificacao()
+        {
+            return Salario;
+        }
+
+        /*public Funcionario(int _tipo)
+        {
+            Tipo = _tipo;
+        }
+        
+        public double GetBonificacao()
+        {
+            switch (Tipo)
+            {
+                case 1:
+                    return Salario;
+                    break;
+                case 2:
+                    return Salario * 0.10;
+                    break;
+                default:
+                    return 0;
+                    break;
+            }
+        }*/
+    }
+}
---
+++ ByteBankMod3/Program.cs
@@ -0,0 +1,33 @@
+﻿using ByteBankMod3.Funcionários;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBankMod3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GerenciadorDeBonificacao gerenciador = new GerenciadorDeBonificacao();
+
+            Funcionario Carlos = new Funcionario();
+
+            Carlos.Nome = "Carlos";
+            Carlos.Cpf = "76357953768";
+            Carlos.Salario = 7500.5;
+
+            gerenciador.Registrar = (Carlos);
+
+            Console.WriteLine(Carlos.Nome);
+            Console.WriteLine(Carlos.GetBonificacao());
+
+
+
+            Console.WriteLine("aperte enter para finalizar : ");
+            Console.ReadLine();
+        }
+    }
+}
